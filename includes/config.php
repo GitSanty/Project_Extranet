27c672f9e1202597d94c0d35cd0bca46f0359faf
Santiago Astete <sastetec@hotmail.com>
2017-08-27 21:32:41 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -17,8 +17,21 @@ define('SITEEMAIL','noreply@domain.com');
 
 try {
 
+	
+	
+	$dsn = "pgsql:"
+    . "host=ec2-54-247-119-245.eu-west-1.compute.amazonaws.com;"
+    . "dbname=d306sj6ieqet92;"
+    . "user=mcsrmexgtpzjgj;"
+    . "port=5432;"
+    . "sslmode=require;"
+    . "password=69504d4d26ef8e273de69ffb401ff3a6048c3528ea6c2deab1629111f5c500d5";
+
+	//$db = new PDO($dsn); new
+	
 	//create PDO connection
-	$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+	//$db = new PDO("mysql:host=".DBHOST.";dbname=".DBNAME, DBUSER, DBPASS);
+	$db = new PDO($dsn);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch(PDOException $e) {
